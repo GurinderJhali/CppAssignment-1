fd1cb9ccda53869becc651971bb29e60cb832867
Stopren123 <71167120+Stopren123@users.noreply.github.com>
2020-09-20 08:00:32 -0700

    Create Q13.cpp

---
+++ W1/Q13.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int speed {20};
+    int time {10};
+    
+    cout<<"Speed is "<<20<<endl;
+    cout<<"Time is " <<10<<endl;
+    cout<<"The distance will be "<< speed * time <<endl;
+    
+    return 0;
+    
+}
